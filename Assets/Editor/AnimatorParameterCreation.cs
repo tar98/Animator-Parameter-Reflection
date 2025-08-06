0bedff611cd21ba24790f973f83cce3126836e31
tar98 <tarchi.giacomo2310@gmail.com>
2025-08-06 12:44:19 +0200

    add Doc and Assembly

--- Assets/Editor/AnimatorParameterCreation.cs
+++ Assets/Editor/AnimatorParameterCreation.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading;
+using System.Threading.Tasks;
 using UnityEditor;
 using UnityEditor.Animations;
 using UnityEngine;
@@ -13,6 +14,7 @@ public class AnimatorParameterCreation : EditorWindow {
 	public List<AnimatorController> animatorControllers;
 	private SerializedObject so;
 	private const string Title = "Create Reflection File";
+	private const int BUFFER_SIZE = 8192;
 
 	private void OnEnable() {
 		string[] paths = Directory.GetFiles("Assets", "*.controller", SearchOption.AllDirectories);
@@ -30,7 +32,10 @@ public class AnimatorParameterCreation : EditorWindow {
 		so.Update();
 		
 		SerializedProperty property = so.FindProperty("animatorControllers");
-		EditorGUILayout.PropertyField(property, new GUIContent("Animator Controller", EditorGUIUtility.IconContent("d_NetworkAnimator Icon").image));
+		EditorGUILayout.PropertyField(
+			property,
+			new GUIContent("Animator Controller", EditorGUIUtility.IconContent("d_NetworkAnimator Icon").image)
+		);
 		
 		so.ApplyModifiedProperties();
 
@@ -44,17 +49,20 @@ public class AnimatorParameterCreation : EditorWindow {
 		EditorGUI.BeginDisabledGroup(disable);
 		if (GUILayout.Button("Create Reflection", GUI.skin.button)) {
 			EditorUtility.ClearProgressBar();
-			Reflection(
+			_ = Reflection(
 				EditorUtility.SaveFilePanel(
 					"Save file",
 					"Assets", 
 					"AnimatorParameter",
 					"cs"
-					)
-				);
+				)
+			);
 		}
 		EditorGUI.EndDisabledGroup();
 	}
+	
+	[MenuItem("Tool/Animator Parameter Creation")]
+	public static void OpenMenu() => GetWindow<AnimatorParameterCreation>("Animator Parameter Creation");
 
 	private void HelpBox() {
 		if (animatorControllers.Count == 0) {
@@ -71,66 +79,82 @@ public class AnimatorParameterCreation : EditorWindow {
 		}
 	}
 
-	private async void Reflection(string path) {
-		if (!string.IsNullOrEmpty(path)) {
-			string fileName = Path.GetFileNameWithoutExtension(path);
-			await using (FileStream stream = File.Create(path, 4096, FileOptions.Asynchronous)) {
-				byte[] bytes = GetTextToByte("using UnityEngine;\n\n");
-				EditorUtility.DisplayProgressBar(Title, "Initialize...", 0);
-				await stream.WriteAsync(bytes, 0, bytes.Length);
-
-				bytes = GetTextToByte(SetClassDeclaration(fileName));
-				EditorUtility.DisplayProgressBar(Title, "Initialize...", 1f);
-				await stream.WriteAsync(bytes, 0, bytes.Length);
-
-				for (int i = 0; i < animatorControllers.Count; i++) {
-					string info = $"Create Parameters : {animatorControllers[i].name} ";
-					
-					bytes = GetTextToByte(SetSubClassDeclaration(animatorControllers[i].name));
-					EditorUtility.DisplayProgressBar(Title, info, 0f);
-					await stream.WriteAsync(bytes, 0, bytes.Length);
-					Thread.Sleep(250);
-					
-					AnimatorControllerParameter[] parameters = animatorControllers[i].parameters;
-					
-					for (int j = 0; j < parameters.Length; j++) {
-						bytes = GetTextToByte(SetParamDeclaration(parameters[j].name));
-						EditorUtility.DisplayProgressBar(Title, info, (float)j / parameters.Length);
-						await stream.WriteAsync(bytes, 0, bytes.Length);
-						Thread.Sleep(200);
-					}
-					
-					bytes = GetTextToByte(CloseSubClassBrace());
-					EditorUtility.DisplayProgressBar(Title, info, 1);
-					await stream.WriteAsync(bytes, 0, bytes.Length);
+	private async Task Reflection(string path) {
+		if (string.IsNullOrEmpty(path)) return;
+		
+		// Get the path file.
+		string fileName = Path.GetFileNameWithoutExtension(path);
+		await using (FileStream stream = File.Create(path, BUFFER_SIZE, FileOptions.Asynchronous)) {
+			const string initInfo = "Initialize...";
+			
+			// Create initial comment
+			await WriteFile(stream, PreGenerateComment(), initInfo, 0f);
+			Thread.Sleep(100);
+			
+			// Create the usings
+			await WriteFile(stream, "using UnityEngine;\n\n", initInfo, 0.5f);
+			Thread.Sleep(100);
+			
+			// Create the abstract class using the same name of the file.
+			await WriteFile(stream, SetClassDeclaration(fileName), initInfo, 1f);
+			Thread.Sleep(100);
+			
+			// Create each parameter
+			for (int i = 0; i < animatorControllers.Count; i++) {
+				// Create each controller class by name
+				string info = $"Create Parameters : {animatorControllers[i].name} ";
+				
+				await WriteFile(stream, SetSubClassDeclaration(animatorControllers[i].name), info, 0f);
+				Thread.Sleep(250);
+				
+				AnimatorControllerParameter[] parameters = animatorControllers[i].parameters;
+				
+				// Create each controller parameter const by name
+				for (int j = 0; j < parameters.Length; j++) {
+					await WriteFile(stream, SetParamDeclaration(parameters[j].name), info, (float)j / parameters.Length);
 					Thread.Sleep(250);
 				}
 				
-				bytes = GetTextToByte(CloseClassBrace());
-				EditorUtility.DisplayProgressBar(Title, "Close File...", 1);
-				await stream.WriteAsync(bytes, 0, bytes.Length);
-				stream.Close();
-				await stream.DisposeAsync();
+				// Close the class controller subclass
+				await WriteFile(stream, CloseSubClassBrace(), info, 1f);
+				Thread.Sleep(250);
 			}
 			
-			EditorUtility.ClearProgressBar();
-			EditorUtility.DisplayDialog(Title, $"Create Animator Reflection \nPath : {Path.GetRelativePath("Assets", path)}", "Okay");
-			AssetDatabase.Refresh();
+			// Close the file and save it
+			await WriteFile(stream, CloseClassBrace(), "Close and save file...", 0f);
+			Thread.Sleep(200);
+			EditorUtility.DisplayProgressBar(Title, "Close and save file...", 1f);
+			Thread.Sleep(100);
+			
+			stream.Close();
+			// no need of "stream.DisposeAsync()" because once the using is closed it'll be called automatically.
 		}
+		
+		EditorUtility.ClearProgressBar();
+		EditorUtility.DisplayDialog(Title, $"Create Animator Reflection \nPath : {Path.GetRelativePath("Assets", path)}", "Okay");
+		AssetDatabase.Refresh();
+	}
+
+#region REFECTION_FUNCTION
+
+	private async Task WriteFile(FileStream file, string data, string progressInfo, float progress) {
+		byte[] bytes = GetTextToByte(data);
+		EditorUtility.DisplayProgressBar(Title, progressInfo, progress);
+		await file.WriteAsync(bytes, 0, bytes.Length);
 	}
 
 	private string SetClassDeclaration(string text) {
-		string str = ReplaceNotValidChar(text);
+		string str = ReplaceNotValidChar(new StringBuilder(text));
 		return $"public abstract class {str} " + "{ \n";
 	}
 	
 	private string SetSubClassDeclaration(string text) {
-		string str = ReplaceNotValidChar(text);
+		string str = ReplaceNotValidChar(new StringBuilder(text));
 		return $"\tpublic abstract class {str} " + "{ \n";
 	}
 	
 	private string SetParamDeclaration(string text) {
-		string str = ReplaceNotValidChar(text);
+		string str = ReplaceNotValidChar(new StringBuilder(text));
 		return $"\t\tpublic static readonly int {str} = Animator.StringToHash(nameof({str}));\n";
 	}
 
@@ -138,23 +162,26 @@ public class AnimatorParameterCreation : EditorWindow {
 	
 	private string CloseSubClassBrace() => "\t}\n\n";
 	
-	private byte[] GetTextToByte(string text) {
-		return Encoding.Default.GetBytes(text);
-	}
-
-	[MenuItem("Tool/Animator Parameter Creation")]
-	public static void OpenMenu() => GetWindow<AnimatorParameterCreation>("Animator Parameter Creation");
+	private byte[] GetTextToByte(string text) => Encoding.Default.GetBytes(text);
 	
-	private string ReplaceNotValidChar(string text) {
-		text = text.Replace(' ', '_');
-		text = text.Replace('-', '_');
-		text = text.Replace('@', '_');
-		text = text.Replace('[', '_');
-		text = text.Replace(']', '_');
-		text = text.Replace('(', '_');
-		text = text.Replace(')', '_');
-		return text;
+	private string ReplaceNotValidChar(StringBuilder text) {
+		const char separator = '_';
+		
+		text = text.Replace(' ', separator);
+		text = text.Replace('-', separator);
+		text = text.Replace('@', separator);
+		text = text.Replace('[', separator);
+		text = text.Replace(']', separator);
+		text = text.Replace('(', separator);
+		text = text.Replace(')', separator);
+		return text.ToString();
+	}
+    
+	private string PreGenerateComment() {
+		return "/////////////////////////////////////////////////////////////////\n" +
+		       "//// \tThis class is generated by the Animator Parameter Reflection\n" +
+		       "/////////////////////////////////////////////////////////////////\n";
 	}
 }
-
+#endregion
 #endif
